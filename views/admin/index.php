1175970247316e750ad80bdc0ce11c6cb3b1f51e
hirohitoqwe <balduin@internet.ru>
2023-07-19 17:45:27 +0300

    change in admin template

--- views/admin/index.php
+++ views/admin/index.php
@@ -1,7 +1,7 @@
 <div class="admin-index">
     <?php foreach ($users as $user): ?>
         <div class="p-4 mb-2">
-            <h3><?= $user->username ?></h3>
+            <h4><?= $user->id . " " . $user->username ?></h4>
         </div>
     <?php endforeach; ?>
 </div>
